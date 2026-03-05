3ced627b11e4ff0e82f2c20a4428ca5207d6eac1
Maksim Sergienko <creativesun@yandex.ru>
2026-03-05 21:59:56 +0300

    feat(types): add color property to character classes for enhanced visual representation

--- src/types/characters.ts
+++ src/types/characters.ts
@@ -7,16 +7,16 @@ export type CharacterClass = {
 };
 
 export const Classes: Array<CharacterClass> = [
-  { name: "Death Knight", icon: classIcons["Death Knight"] },
-  { name: "Druid", icon: classIcons.Druid },
-  { name: "Hunter", icon: classIcons.Hunter },
-  { name: "Mage", icon: classIcons.Mage },
-  { name: "Paladin", icon: classIcons.Paladin },
-  { name: "Priest", icon: classIcons.Priest },
-  { name: "Rogue", icon: classIcons.Rogue },
-  { name: "Shaman", icon: classIcons.Shaman },
-  { name: "Warlock", icon: classIcons.Warlock },
-  { name: "Warrior", icon: classIcons.Warrior },
+  { name: "Death Knight", icon: classIcons["Death Knight"], color: 'C41E3A'},
+  { name: "Druid", icon: classIcons.Druid, color: 'FF7C0A' },
+  { name: "Hunter", icon: classIcons.Hunter, color: 'AAD372' },
+  { name: "Mage", icon: classIcons.Mage, color: '3FC7EB' },
+  { name: "Paladin", icon: classIcons.Paladin, color: 'F48CBA' },
+  { name: "Priest", icon: classIcons.Priest, color: 'FFFFFF' },
+  { name: "Rogue", icon: classIcons.Rogue, color: 'FFF468' },
+  { name: "Shaman", icon: classIcons.Shaman, color: '0070DD' },
+  { name: "Warlock", icon: classIcons.Warlock, color: '8788EE' },
+  { name: "Warrior", icon: classIcons.Warrior, color: 'C69B6D' }
 ];
 
 export type Character = {
