12250394a62c1e78940e978a0d5978622fa97add
Maksim Sergienko <creativesun@yandex.ru>
2026-03-05 16:54:17 +0300

    feat(app): implement dungeon table display with dynamic data rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import './App.css'
+import { DungeonList } from "./data/dungeons.ts";
+import "./App.css";
 
 function App() {
   return (
-    <header>
-      <h1>My Raid CDs</h1>
-    </header>
-  )
+    <>
+      <header>
+        <h1>My Raid CDs</h1>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+            <th>Item Level</th>
+            <th>Mode</th>
+          </tr>
+        </thead>
+        <tbody>
+          {DungeonList.map((dungeon, i) => (
+            <tr key={i}>
+              <td>{dungeon.name}</td>
+              <td>{dungeon.size}</td>
+              <td>{dungeon.itemLevel.join("...")}</td>
+              <td>{dungeon.mode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App
