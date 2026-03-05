54e8887e9262f6ea307fbff378daa9b4f599393f
Maksim Sergienko <creativesun@yandex.ru>
2026-03-05 18:44:58 +0300

    feat(assets): add TypeScript module for class icons mapping

---
+++ src/assets/class-icons/class-icons.ts
@@ -0,0 +1,23 @@
+import deathKnightIcon from "./death-knight.png";
+import druidIcon from "./druid.png";
+import hunterIcon from "./hunter.png";
+import mageIcon from "./mage.png";
+import paladinIcon from "./paladin.png";
+import priestIcon from "./priest.png";
+import rogueIcon from "./rogue.png";
+import shamanIcon from "./shaman.png";
+import warlockIcon from "./warlock.png";
+import warriorIcon from "./warrior.png";
+
+export const classIcons = {
+  "Death Knight": deathKnightIcon,
+  Druid: druidIcon,
+  Hunter: hunterIcon,
+  Mage: mageIcon,
+  Paladin: paladinIcon,
+  Priest: priestIcon,
+  Rogue: rogueIcon,
+  Shaman: shamanIcon,
+  Warlock: warlockIcon,
+  Warrior: warriorIcon,
+} as const;
