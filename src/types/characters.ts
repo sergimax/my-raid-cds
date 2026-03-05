1c07bcd875db0ef3cc536e3c8fe289eacdc76804
Maksim Sergienko <creativesun@yandex.ru>
2026-03-05 18:52:50 +0300

    refactor(types): update CharacterClass to require icon and integrate class icons mapping

--- src/types/characters.ts
+++ src/types/characters.ts
@@ -1,40 +1,22 @@
+import { classIcons } from "../assets/class-icons/class-icons";
+
 export type CharacterClass = {
   name: string;
   color?: string;
-  icon?: string;
+  icon: string;
 };
 
 export const Classes: Array<CharacterClass> = [
-  {
-    name: "Death Knight",
-  },
-  {
-    name: "Druid",
-  },
-  {
-    name: "Hunter",
-  },
-  {
-    name: "Mage",
-  },
-  {
-    name: "Paladin",
-  },
-  {
-    name: "Priest",
-  },
-  {
-    name: "Rogue",
-  },
-  {
-    name: "Shaman",
-  },
-  {
-    name: "Warlock",
-  },
-  {
-    name: "Warrior",
-  },
+  { name: "Death Knight", icon: classIcons["Death Knight"] },
+  { name: "Druid", icon: classIcons.Druid },
+  { name: "Hunter", icon: classIcons.Hunter },
+  { name: "Mage", icon: classIcons.Mage },
+  { name: "Paladin", icon: classIcons.Paladin },
+  { name: "Priest", icon: classIcons.Priest },
+  { name: "Rogue", icon: classIcons.Rogue },
+  { name: "Shaman", icon: classIcons.Shaman },
+  { name: "Warlock", icon: classIcons.Warlock },
+  { name: "Warrior", icon: classIcons.Warrior },
 ];
 
 export type Character = {
