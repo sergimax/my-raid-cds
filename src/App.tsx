1c9472ccda030f1a5fd411658f4ad289cea181df
Maksim Sergienko <creativesun@yandex.ru>
2026-03-05 17:14:45 +0300

    feat(app): enhance dungeon display by formatting names with mode suffix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { DungeonList } from "./data/dungeons.ts";
+import { DungeonMode } from "./types/dungeons.ts";
 import "./App.css";
+
+function formatName(dungeon: (typeof DungeonList)[number]): string {
+  const modeSuffix = dungeon.mode === DungeonMode.HEROIC ? " 💀" : "";
+  return `${dungeon.name} ${dungeon.size}${modeSuffix}`;
+}
 
 function App() {
   return (
@@ -11,18 +17,14 @@ function App() {
         <thead>
           <tr>
             <th>Name</th>
-            <th>Size</th>
             <th>Item Level</th>
-            <th>Mode</th>
           </tr>
         </thead>
         <tbody>
           {DungeonList.map((dungeon, i) => (
             <tr key={i}>
-              <td>{dungeon.name}</td>
-              <td>{dungeon.size}</td>
+              <td>{formatName(dungeon)}</td>
               <td>{dungeon.itemLevel.join("...")}</td>
-              <td>{dungeon.mode}</td>
             </tr>
           ))}
         </tbody>
