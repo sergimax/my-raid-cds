09b6e62682884b0b30061d83e1eb7e98e757b681
Maksim Sergienko <creativesun@yandex.ru>
2026-03-05 22:00:33 +0300

    feat(app): add class list display with icons for character classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { DungeonList } from "./data/dungeons.ts";
+import { Classes } from "./types/characters.ts";
 import { DungeonMode } from "./types/dungeons.ts";
 import "./App.css";
 
@@ -8,6 +9,18 @@ function App() {
       <header>
         <h1>My Raid CDs</h1>
       </header>
+      <ul className="class-list">
+        {Classes.map((c) => (
+          <li
+            key={c.name}
+            className="class-item"
+            style={{ color: c.color ? `#${c.color}` : undefined }}
+          >
+            <img src={c.icon} alt="" className="class-icon" />
+            <span>{c.name}</span>
+          </li>
+        ))}
+      </ul>
       <table>
         <thead>
           <tr>
