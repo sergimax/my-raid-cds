2ce683c02b9907346ad3f2279d17b3eb9c63e8f0
Maksim Sergienko <creativesun@yandex.ru>
2026-04-05 18:20:22 +0300

    feat(app-footer): create AppFooter component with styles and integrate into main application

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { AppFooter } from "./app-footer/index.tsx";
 export { CharacterForm } from "./character-form/index.tsx";
 export { DungeonForm } from "./dungeon-form/index.tsx";
 export { DungeonTable } from "./dungeon-table/index.tsx";
