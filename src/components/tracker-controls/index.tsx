db7599fbd1892781f5c35beced03ed877e75b88c
Maksim Sergienko <creativesun@yandex.ru>
2026-06-01 23:42:13 +0300

    feat(tracker-controls): implement action management for tracker controls with dynamic rendering

--- src/components/tracker-controls/index.tsx
+++ src/components/tracker-controls/index.tsx
@@ -1,20 +1,19 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import {
   Box,
-  Button,
   IconButton,
   Menu,
-  MenuItem,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { useCallback, useId, useState, type MouseEvent } from "react";
+import { useCallback, useId, useMemo, useState, type MouseEvent } from "react";
 import { useRaidTrackerContext } from "../../hooks/use-raid-tracker-context.ts";
+import { buildTrackerActions } from "./actions.ts";
+import { renderTrackerAction } from "./render-tracker-action.tsx";
 
 export function TrackerControls() {
   const tracker = useRaidTrackerContext();
-  const showAddFromTemplate = tracker.dungeons.length === 0;
-  const resetAllTogglesDisabled = !tracker.canResetAllToggles;
+  const actions = useMemo(() => buildTrackerActions(tracker), [tracker]);
 
   const theme = useTheme();
   const menuLayout = useMediaQuery(theme.breakpoints.down("md"));
@@ -50,44 +49,9 @@ export function TrackerControls() {
           anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
           transformOrigin={{ vertical: "top", horizontal: "right" }}
         >
-          {showAddFromTemplate ? (
-            <MenuItem
-              onClick={() => {
-                tracker.handleAddFromTemplate();
-                closeMenu();
-              }}
-            >
-              Add from template
-            </MenuItem>
-          ) : null}
-          <MenuItem
-            selected={tracker.showCharacterForm}
-            onClick={() => {
-              tracker.toggleCharacterForm();
-              closeMenu();
-            }}
-          >
-            Add character
-          </MenuItem>
-          <MenuItem
-            selected={tracker.showDungeonForm}
-            onClick={() => {
-              tracker.toggleDungeonForm();
-              closeMenu();
-            }}
-          >
-            Add dungeon
-          </MenuItem>
-          <MenuItem
-            disabled={resetAllTogglesDisabled}
-            onClick={() => {
-              tracker.handleResetAllToggles();
-              closeMenu();
-            }}
-            sx={{ color: resetAllTogglesDisabled ? undefined : "warning.main" }}
-          >
-            Reset all toggles
-          </MenuItem>
+          {actions.map((action) =>
+            renderTrackerAction(action, "menuItem", { onAfterClick: closeMenu }),
+          )}
         </Menu>
       </>
     );
@@ -105,43 +69,7 @@ export function TrackerControls() {
         minWidth: 0,
       }}
     >
-      {showAddFromTemplate ? (
-        <Button
-          size="small"
-          variant="contained"
-          color="secondary"
-          onClick={tracker.handleAddFromTemplate}
-        >
-          Add from template
-        </Button>
-      ) : null}
-      <Button
-        size="small"
-        variant={tracker.showCharacterForm ? "contained" : "outlined"}
-        color="inherit"
-        aria-expanded={tracker.showCharacterForm}
-        onClick={tracker.toggleCharacterForm}
-      >
-        Add character
-      </Button>
-      <Button
-        size="small"
-        variant={tracker.showDungeonForm ? "contained" : "outlined"}
-        color="inherit"
-        aria-expanded={tracker.showDungeonForm}
-        onClick={tracker.toggleDungeonForm}
-      >
-        Add dungeon
-      </Button>
-      <Button
-        size="small"
-        variant="text"
-        color="warning"
-        disabled={resetAllTogglesDisabled}
-        onClick={tracker.handleResetAllToggles}
-      >
-        Reset all toggles
-      </Button>
+      {actions.map((action) => renderTrackerAction(action, "button"))}
     </Box>
   );
 }
