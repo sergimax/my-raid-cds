b24f4ac53740089c5497d6aa6e002c8cf6b829fc
Maksim Sergienko <creativesun@yandex.ru>
2026-07-17 16:46:55 +0300

    feat(tracker-toolbar-panel): add gear pick panel handling and update related logic

--- src/components/tracker-toolbar-panel/resolve-toolbar-panel-id.ts
+++ src/components/tracker-toolbar-panel/resolve-toolbar-panel-id.ts
@@ -1,12 +1,13 @@
 export type MainToolbarPanelId = "character" | "dungeon" | "bis";
 
-export type ToolbarPanelId = MainToolbarPanelId | "export";
+export type ToolbarPanelId = MainToolbarPanelId | "export" | "gear";
 
 export function resolveToolbarPanelId(options: {
   showCharacterForm: boolean;
   showDungeonForm: boolean;
   showBisListsPanel: boolean;
   showExportPanel: boolean;
+  showGearPickPanel: boolean;
 }): ToolbarPanelId | null {
   if (options.showCharacterForm) {
     return "character";
@@ -16,6 +17,9 @@ export function resolveToolbarPanelId(options: {
   }
   if (options.showExportPanel) {
     return "export";
+  }
+  if (options.showGearPickPanel) {
+    return "gear";
   }
   if (options.showBisListsPanel) {
     return "bis";
